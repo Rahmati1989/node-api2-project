beadc052edd417bfc2b38345465df811273bf9d0
Rafiullah Rahmati <rafiullahrahmati@Rafiullahs-MacBook-Pro.local>
2021-02-14 23:15:40 -0800

    Git Post Done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,19 @@
 // implement your posts router here
+
+const Posts = require('./posts-model');
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res) =>{
+    Posts.find()
+    .then((posts) =>{
+        res.status(200).json(posts);
+    })
+    .catch((error) =>{
+        res.status(500).json({message: 'The posts information could not be retrieved'})
+    });
+});
+
+
+module.exports = router;
